38f17c769bd0f7efebd030bcdda7cf831a5e3679
DomNolt <36490339+DomNolt@users.noreply.github.com>
2019-02-27 09:38:19 -0500

    The main files for the project so far
    Basic framework of the navigation including the fragments and bottom navigation.  Login was just one of the preset activities and I need to go through and remove all the unnecessary stuff.

---
+++ main/java/com/commonsense/seniorproject/StoreFragment.java
@@ -0,0 +1,24 @@
+package com.commonsense.seniorproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+import android.app.Fragment;
+
+public class StoreFragment extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_store, container, false);
+        // get the reference of Button
+
+        return view;
+    }
+}
