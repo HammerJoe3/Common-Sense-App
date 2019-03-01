bca029188cc2178f4f92fb30d4fe5a8e3dd41630
Joseph Hammer <joey24854@hotmail.com>
2019-02-28 21:19:09 -0500

    Added Store Code

--- main/java/com/commonsense/seniorproject/StoreFragment.java
+++ main/java/com/commonsense/seniorproject/StoreFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.Toast;
 import android.app.Fragment;
@@ -15,10 +18,19 @@ public class StoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_store, container, false);
         // get the reference of Button
 
+        WebView myWebView = (WebView) view.findViewById(R.id.webview);
+        myWebView.loadUrl("https://smartcommonsense.com/shop/");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+
         return view;
     }
+
+
 }
