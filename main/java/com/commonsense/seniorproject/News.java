d3dcd20701f3f8fccd924fcc89effe272c3db7a9
Dylanshapiro <43762253+Dylanshapiro@users.noreply.github.com>
2019-05-09 23:56:30 -0400

    added comments

--- main/java/com/commonsense/seniorproject/News.java
+++ main/java/com/commonsense/seniorproject/News.java
@@ -1,36 +1,52 @@
 package com.commonsense.seniorproject;
+/*
+Created By Dylan Shapiro 
 
+*/
 
 public class News {
     String link;
     String articleName;
     String date;
-
-    @Override
-    public String toString() {
-        return "News [link=" + link + ", articleName=" + articleName + "]";
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public String getArticleName() {
-        return articleName;
-    }
-
-    public void setArticleName(String articleName) {
-        this.articleName = articleName;
-    }
-
+    /*
+    constructor for news takes link name and date crawled
+    */
     public News(String link, String articleName, String date) {
         this.link = link;
         this.articleName = articleName;
         this.date = date;
     }
+    /*
+    to string method prints out article name and link
+    */
+    @Override
+    public String toString() {
+        return "News [link=" + link + ", articleName=" + articleName + "]";
+    }
+    /*
+    returns link
+    */
+    public String getLink() {
+        return link;
+    }
+    /*
+    sets the link
+    */
+    public void setLink(String link) {
+        this.link = link;
+    }
+    /*
+    gets the article name
+    */
+    public String getArticleName() {
+        return articleName;
+    }
+    /*
+    sets the article name 
+    */
+    public void setArticleName(String articleName) {
+        this.articleName = articleName;
+    }
+    
 
 }
