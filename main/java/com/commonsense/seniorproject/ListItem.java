cb9e5ce84a941bfe1ac2320ca6ea214bc368be19
Dylanshapiro <43762253+Dylanshapiro@users.noreply.github.com>
2019-05-09 23:47:26 -0400

    added comments

--- main/java/com/commonsense/seniorproject/ListItem.java
+++ main/java/com/commonsense/seniorproject/ListItem.java
@@ -1,6 +1,11 @@
 package com.commonsense.seniorproject;
+/*
+Created By Dylan Shapiro 
+
+*/
 
 
+// simple class used for listview in widget
 public class ListItem {
     public String heading,content;
 
